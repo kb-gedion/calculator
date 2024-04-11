2ecc729874e6061b6d209ddea277ca5e6e7eb4ee
Kebur Lulseged <kb.gedion@gmail.com>
2024-04-11 11:18:29 -0700

    Make calculator buttons functional with single digit calculations

--- script.js
+++ script.js
@@ -1,28 +1,68 @@
 let firstNum;
 let secondNum;
-let operator;
+let operator = null;
 
+const keypad = document.querySelector(".bottomHalf");
+const display = document.querySelector(".onDisplay");
 
+function reset() {
+    firstNum = null;
+    secondNum = null;
+    operator = null;
+}
+
+keypad.addEventListener("click", (e) => {
+    let pressed = e.target.textContent;
+    display.textContent = pressed;
+
+    if (pressed == 'AC') {
+        display.textContent = "";
+        reset();
+    } else if (isNumber(pressed)) {
+        if (operator === null) {
+            firstNum = pressed;
+        }
+        else if (operator) {
+            secondNum = pressed;
+        }
+    } else if (pressed === '/' || pressed === '-' || pressed === '+' || pressed === 'x') {
+        if (!firstNum) {
+            display.textContent = "ERR /must have number"
+            reset();
+        } else {
+            operator = pressed;
+        }
+    } else if (pressed === '=') {
+        if (firstNum && secondNum && operator) {
+            display.textContent = operate(operator, firstNum, secondNum);
+        } else {
+            display.textContent = "ERR /invalid operation";
+            reset();
+        }
+    }
+    
+
+})
 
 const add = function(x, y) {
-    return x + y;
+    return +x + +y;
 };
   
 const subtract = function(x, y) {
-    return x - y;
+    return +x - +y;
 };
 
 const sum = function(...arr) {
-return arr.reduce((total, current) => (total + current), 0);
+return arr.reduce((total, current) => (+total + +current), 0);
 
 };
 
 const multiply = function(...arr) {
-return arr.reduce((total, current) => (total * current));
+return arr.reduce((total, current) => (+total * +current));
 };
 
 const divide = function(...arr) {
-    return arr.reduce((total, current) => (total / current));
+    return arr.reduce((total, current) => (+total / +current));
 }
 
 function operate(op, a, b) {
@@ -33,7 +73,7 @@ function operate(op, a, b) {
         case '-':
             return subtract(a, b); 
             break;
-        case '*':
+        case 'x':
             return multiply(a, b);
             break;
         case '/':
@@ -42,4 +82,8 @@ function operate(op, a, b) {
         default:
             return "ERROR";
     }
+}
+
+function isNumber(pressed) {
+    return (+pressed >= 0 && +pressed <= 9)
 }
