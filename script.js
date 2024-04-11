7eb83813933a69c349e5f4347ee78b5576b65772
Kebur Lulseged <kb.gedion@gmail.com>
2024-04-11 08:10:23 -0700

    Create operate function to handle which operation to run

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let firstNum;
+let secondNum;
+let operator;
+
+
+
 const add = function(x, y) {
     return x + y;
 };
@@ -17,4 +23,23 @@ return arr.reduce((total, current) => (total * current));
 
 const divide = function(...arr) {
     return arr.reduce((total, current) => (total / current));
+}
+
+function operate(op, a, b) {
+    switch (op) {
+        case '+':
+            return sum(a,b);
+            break;
+        case '-':
+            return subtract(a, b); 
+            break;
+        case '*':
+            return multiply(a, b);
+            break;
+        case '/':
+            return divide(a, b);
+            break;
+        default:
+            return "ERROR";
+    }
 }
